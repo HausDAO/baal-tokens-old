b7f4aa5d09e66954894b698914b35446f633a295
dekanbro <dekanbrown@odyssy.io>
2023-08-17 11:18:57 -0600

    first commit

---
+++ contracts/interfaces/IShaman.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IShaman {
+    function setup(address dao, address vault, bytes memory initParams)
+        external;
+}
---
+++ contracts/lootTokens/FixedLoot.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+error NotSupported();
+error AlreadyMinted();
+
+
+contract FixedLoot is
+    ERC20SnapshotUpgradeable,
+    ERC20PermitUpgradeable,
+    OwnableUpgradeable
+{
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @notice Configure loot - called by Baal on summon
+    /// @dev initializer should prevent this from being called again
+    /// @param name_ Name for ERC20 token trackers
+    /// @param symbol_ Symbol for ERC20 token trackers
+    function setUp(string memory name_, string memory symbol_)
+        external
+        initializer
+    {
+        require(bytes(name_).length != 0, "loot: name empty");
+        require(bytes(symbol_).length != 0, "loot: symbol empty");
+
+        __ERC20_init(name_, symbol_);
+        __ERC20Permit_init(name_);
+        __ERC20Snapshot_init();
+        __Ownable_init();
+
+    }
+
+    /// @notice Allows baal to create a snapshot
+    function snapshot() external onlyOwner returns(uint256) {
+        return _snapshot();
+    }
+
+    /// @notice get current SnapshotId
+    function getCurrentSnapshotId() external view returns (uint256) {
+        return _getCurrentSnapshotId();
+    }
+
+    /// @notice Baal-only function to pause shares.
+    function pause() public view onlyOwner {
+        revert NotSupported();
+    }
+
+    /// @notice Baal-only function to pause shares.
+    function unpause() public view onlyOwner {
+        revert NotSupported();
+    }
+
+    /// @notice Baal-only function to mint loot.
+    /// @param recipient Address to receive loot
+    /// @param amount Amount to mint
+    function mint(address recipient, uint256 amount) external view onlyOwner {
+        revert NotSupported();    
+    }
+
+    function initialMint(uint256 initialSupply, address initialHolder) external onlyOwner {
+        // TODO should only happen dureing init by factory
+        if (totalSupply() > 0) revert AlreadyMinted();
+        _mint(initialHolder, initialSupply);    
+    }
+
+    /// @notice Baal-only function to burn loot.
+    /// @param account Address to lose loot
+    /// @param amount Amount to burn
+    function burn(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
+    }
+
+    /// @notice Internal hook to restrict token transfers unless allowed by baal
+    /// @dev Allows transfers if msg.sender is Baal which enables minting and burning
+    /// @param from The address of the source account.
+    /// @param to The address of the destination account.
+    /// @param amount The number of `loot` tokens to transfer.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Upgradeable, ERC20SnapshotUpgradeable) {
+        super._beforeTokenTransfer(from, to, amount);
+        require(
+                (msg.sender == owner() && to == address(0)), /*Burning by Baal allowed*/
+            "loot: !transferable"
+        );
+    }
+
+}
